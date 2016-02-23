54caca27d2931c2feeed587f4326df0065e2420b
Yang14 <359097729@qq.com>
2016-02-23 21:51:08 +0800

    测试出ssdb支持多个客户端的并发访问，性能比使用rmi后要高出很多

--- src/test/java/backend/TestMultiSSDB.java
+++ src/test/java/backend/TestMultiSSDB.java
@@ -23,13 +23,14 @@ import java.util.concurrent.CountDownLatch;
 public class TestMultiSSDB {
     private static Logger logger = LoggerFactory.getLogger("TestMultiSSDB");
 
-    private int threadCount = 8;
+    private int threadCount = 1;
+    private CountDownLatch totalLatch = new CountDownLatch(3);
     private CountDownLatch latch = new CountDownLatch(threadCount);
 
     private Map<String, Long> createTimeMap = new ConcurrentHashMap<String, Long>();
     private Map<String, Long> findTimeMap = new ConcurrentHashMap<String, Long>();
 
-    private final int createCount = 100000 / threadCount;
+    private final int createCount = 1000000 / threadCount;
 
     private Map<Integer, Integer> dCodeMap;
     private static SSDB ssdb = SSDBs.pool("127.0.0.1", PortEnum.SSDB_PORT, 10000, null);
@@ -42,12 +43,21 @@ public class TestMultiSSDB {
         }
     }
 
+    @Test
+    public void testPerformance() throws InterruptedException {
+        testMultiCreate();
+        testMultiListDir();
+        testMultiFindFile();
+        totalLatch.await();
+        logger.info("test finished...");
+    }
+
     /**
      * 测试多个线程并发创建100w索引数据需要的时间
      * 通过rmi接口
      */
     @Test
-    public void testMultiCreate() throws InterruptedException, RemoteException {
+    public void testMultiCreate() throws InterruptedException {
         long start = System.currentTimeMillis();
         for (int i = 0; i < threadCount; ++i) {
             new Thread(new CreateIndexWithNet(), dCodeMap.get(i) + "").start();
@@ -55,7 +65,8 @@ public class TestMultiSSDB {
         latch.await();
         long end = System.currentTimeMillis();
         logger.info(String.format("with net: %s thread create %s mdAttr, use time: %sms", threadCount, createCount, (end - start)));
-        // logger.info("each thread create index time spend is:" + createTimeMap);
+        logger.info("each thread create index time spend is:" + createTimeMap);
+        totalLatch.countDown();
     }
 
     /**
@@ -63,53 +74,89 @@ public class TestMultiSSDB {
      * 通过rmi接口
      */
     @Test
-    public void testMultiFind() throws InterruptedException {
+    public void testMultiListDir() throws InterruptedException {
         long start = System.currentTimeMillis();
         for (int i = 0; i < threadCount; ++i) {
-            new Thread(new FindIndexWithNet(), dCodeMap.get(i) + "").start();
+            new Thread(new ListDir(), dCodeMap.get(i) + "").start();
         }
         latch.await();
         long end = System.currentTimeMillis();
         logger.info(String.format("%s thread find %s mdAttr, use time: %sms", threadCount, createCount, (end - start)));
         logger.info("each thread find index time spend is:" + findTimeMap);
+        totalLatch.countDown();
     }
 
     @Test
-    public void testHSet(){
+    public void testMultiFindFile() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < threadCount; ++i) {
+            new Thread(new FindFileMd(), dCodeMap.get(i) + "").start();
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        logger.info(String.format("%s thread find %s mdAttr, use time: %sms", threadCount, createCount, (end - start)));
+        logger.info("each thread find index time spend is:" + findTimeMap);
+        totalLatch.countDown();
+    }
+
+
+    @Test
+    public void testHSet() {
         long dCode = Long.parseLong(Thread.currentThread().getName());
         long start = System.currentTimeMillis();
         for (int i = 0; i < createCount; i++) {
             ssdb.hset(dCode, "file" + i, JSON.toJSONString(getMdAttr("file" + i, i, false)));
         }
         long end = System.currentTimeMillis();
-        logger.info(String.format("single thread get %sw mdattr, use time: %sms", createCount, (end - start)));
+        createTimeMap.put(dCode + "", end - start);
     }
+
     @Test
-    public void testHGet(){
+    public void testGetMdAttr() {
+        long dCode = Long.parseLong(Thread.currentThread().getName());
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < createCount; i++) {
+            JSON.parseObject(ssdb.hget(dCode, "file" + i).asString());
+        }
+        long end = System.currentTimeMillis();
+        findTimeMap.put(dCode + "", end - start);
+    }
+
+    @Test
+    public void testHGet() {
         long dCode = Long.parseLong(Thread.currentThread().getName());
         Response response = ssdb.hgetall(dCode);
         Map<String, String> mdAttrMap = response.mapString();
-        int i=10;
-        for (String key : mdAttrMap.keySet()){
-            if (i-- == 0) break;
-            logger.info(JSON.parseObject(mdAttrMap.get(key),MdAttr.class).toString());
+        for (String key : mdAttrMap.keySet()) {
+            JSON.parseObject(mdAttrMap.get(key), MdAttr.class);
         }
     }
+
     class CreateIndexWithNet implements Runnable {
 
         @Override
         public void run() {
-                testHSet();;
-                latch.countDown();
+            testHSet();
+            ;
+            latch.countDown();
         }
     }
 
-    class FindIndexWithNet implements Runnable {
+    class ListDir implements Runnable {
 
         @Override
         public void run() {
-                testHGet();
-                latch.countDown();
+            testHGet();
+            latch.countDown();
+        }
+    }
+
+    class FindFileMd implements Runnable {
+
+        @Override
+        public void run() {
+            testGetMdAttr();
+            latch.countDown();
         }
     }
 
